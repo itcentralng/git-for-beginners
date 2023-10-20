5e8133658ec9b6fe176f3875c720c932ae076dba
Nasir Mustapha <teboy.com@gmail.com>
2023-10-20 12:07:46 +0100

    Correction for example2.py

--- example2.py
+++ example2.py
@@ -1,5 +1,11 @@
 test = "Something"
 
 # Exercise:
-# 1. Write 5 examples of bad variable names
-# 2. Write 5 examples of good variable names
+# 1. Write 1 examples of bad variable names
+# 2. Write 1 examples of good variable names
+
+# Corrections
+# 1.
+1name = "Ahmad"
+# 2.
+name = "Ahmad"
