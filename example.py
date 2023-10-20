725711ef9c849c27b55731666cbf681da0748cf4
Nasir Mustapha <teboy.com@gmail.com>
2023-10-20 12:07:37 +0100

    Corrections for example.py

--- example.py
+++ example.py
@@ -3,3 +3,9 @@ print("Test")
 # Assignment:
 # 1. Use print to display your name on the terminal
 # 2. Use print to display your age on the terminal
+
+# Corrections:
+# 1. 
+print("Nasir Mustapha")
+# 2.
+print("10")
